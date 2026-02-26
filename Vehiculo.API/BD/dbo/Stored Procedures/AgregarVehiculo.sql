fdd674ed81188017eff8068c6bf9d4c4b2a41e4d
sandfall74 <adrianpiedrasan@gmail.com>
2026-02-25 19:52:28 -0600

    commit

--- Vehiculo.API/BD/dbo/Stored Procedures/AgregarVehiculo.sql
+++ Vehiculo.API/BD/dbo/Stored Procedures/AgregarVehiculo.sql
@@ -11,7 +11,7 @@
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
+	-- interfering with SELECT statements.--
 	SET NOCOUNT ON;
 
     -- Insert statements for procedure here
